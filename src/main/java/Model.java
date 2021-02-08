399cf99bfec3879a53c3262f3ca2721c3091bf88
Stanislav Stoianov <stanis.stoyanov@gmail.com>
2021-02-08 22:14:56 +0300

    implemented recognition transfer

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 
 public class Model {
 
-    private final double[][] array = new double[1][784];
     private static volatile Model instance;
     private MultiLayerNetwork model;
 
@@ -25,29 +24,30 @@ public class Model {
 
     private Model() {
         try {
-            MultiLayerNetwork model = KerasModelImport.
+            this.model = KerasModelImport.
                     importKerasSequentialModelAndWeights(
                             "/home/stanislav/IdeaProjects/mnist-recognition/src/main/resources/model/model.h5");
-            model.init();
-            this.model = model;
         } catch (InvalidKerasConfigurationException | UnsupportedKerasConfigurationException | IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
 
-    public int recognize(double[][] doubles) {
-        for (int i = 0; i < doubles.length; i++) {
-            for (int j = 0; j < doubles[i].length; j++) {
-                this.array[0][i + (j * doubles.length)] = doubles[i][j];
+    public int recognize(double[][] originalImage) {
+        double[][] compressedImage = new double[28][28];
+        for (int i = 0; i < 28; i++) {
+            for (int j = 0; j < 28; j++) {
+                compressedImage[i][j] = originalImage[i * 10][j * 10];
             }
         }
-        return model.predict(new NDArray(this.array))[0];
-    }
-
-    public int recognize(double[] doubles) {
-        System.arraycopy(doubles, 0, this.array[0], 0, this.array.length);
-        return model.predict(new NDArray(this.array))[0];
+        double[][] array = new double[1][784];
+        int k = 0;
+        for (int i = 0; i < 28; i++) {
+            for (int j = 0; j < 28; j++) {
+                array[0][k++] = compressedImage[j][i];
+            }
+        }
+        return model.predict(new NDArray(array))[0];
     }
 
 }
